6ded6a171efe43b99d631dd8508807acdce64636
saidhruthi <nsaidhruthi@gmail.com>
2018-10-05 10:24:12 +0530

    "check_style_score -> 0 of 3 passed. style score: 0/1 "

---
+++ Sorted_Array/Solution.java
@@ -0,0 +1,66 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     int[] a;
+     int[] b;
+     int n,m;
+     Scanner sc=new Scanner(System.in);
+     System.out.println("enter number of elements in first array");
+     n=sc.nextInt();
+     System.out.println("enter number of elements in second array");
+     m=sc.nextInt();
+     System.out.println("enter array elements");
+     a=new int[n];
+     b=new int[m];
+     for(int i=0;i<n;i++)
+     {
+    	 //System.out.println("a["+i+"]");
+    	 a[i]=sc.nextInt();
+     }
+     for(int j=0;j<m;j++)
+     {
+    	// System.out.println("b["+j+"]");
+    	 b[j]=sc.nextInt();
+     }
+     int[] c;
+     int i=0,j=0,k=0;
+     c=new int[m+n];
+     while(i<a.length&&j<b.length)
+     {
+	   if(a[i]>b[j])
+	   {
+		   c[k]=b[j];
+		   k++;
+		   j++;
+	   }
+	   else
+	   {
+		   c[k]=a[i];
+		   k++;
+		   i++;
+	   }
+      }
+     while(i<a.length)
+     {
+    	 c[k]=a[i];
+    	 k++;
+    	 i++;
+     }
+     while(j<b.length)
+     {
+    	 c[k]=b[j];
+    	 k++;
+    	 j++;
+     }
+     //System.out.println("after sorting");
+     for(k=0;k<c.length;k++)
+     {
+    	 System.out.print(c[k]);
+     }
+	}
+
+}
