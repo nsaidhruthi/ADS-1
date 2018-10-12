4e2b35329ab4d983f4e1da3aed2f0787e2fbe09d
saidhruthi <nsaidhruthi@gmail.com>
2018-10-12 15:43:47 +0530

    "check_style_score -> 2 of 2 passed. style score: 0/1 "

--- Steque (1)/7.1 Steque/Solution.java
+++ Steque (1)/7.1 Steque/Solution.java
@@ -1,128 +1,201 @@
 import java.util.Scanner;
 
-class Node
-{
-	int item;
-	Node next ;
-	Node(int a)
+
+public class Solution{
+	public static void main(String args[])
 	{
-		this.item=a;
+Scanner s=new Scanner(System.in);
+int n=s.nextInt();
+String st1[]=new String[2];
+String st=null;
+int i;
+for(i=0;i<=n;i++)
+{
+	Linkl l=new Linkl();
+
+
+	do
+	{
+		st=s.nextLine();
+		
+		
+		//	System.out.println(st);
+			st1=st.split(" ");
+			switch(st1[0])
+			{
+			case "push":
+		                     l.pushfront(Integer.parseInt(st1[1]));
+		                     l.display();
+
+	System.out.println();
+			                 break;
+			case "enqueue":
+		                     l.pushend(Integer.parseInt(st1[1]));   
+		                     l.display();
+
+	System.out.println();
+		                     break;
+			case "pop":l.popfront();
+							l.display();
+
+	System.out.println();
+			                 break;
+			//default:break;
+		   
+		                
+			}
+			
+		}while(s.hasNext() && st.length()!=0);
+if(i!=0)
+	{
+System.out.println();
+	}
+	
+
+}
 	}
 }
-class Link
+class Linkl{
+Node head;
+
+public void pushend(int a)
+	{
+		Node n=new Node(a);
+	
+		if(head==null)
+		{
+			head=n;
+			
+		}
+		else
+		{	Node temp =head;
+		while(temp.next!=null)
+		{
+			temp=temp.next;
+		}
+		
+		temp.next=n;
+		
+		}
+		
+	}
+	public int  popend()
 {
-	Node first=null;
-	public boolean isEmpty()
-	{
-		return first==null;
-	}
-	public void push(int item)
-	{
-		Node n=new Node(item);
+		Node temp1=null;
+	
+	if(head==null)
 		
-		if(first==null)
+	{
+		return 0;
+	}
+	else if(head.next==null)
+	{Node temp=head;
+	head=null;
+		return temp.data;
+	}
+	else
+	{
+		Node temp=head;
+	
+	while(temp.next.next!=null)
+	{
+		temp=temp.next;
+	}
+	temp1=temp.next;
+	temp.next=null;
+	return temp1.data;
+	}
+	
+
+}
+
+public void display()
+{	
+	Node temp=head;
+	if(head==null)
+	{
+		System.out.print("Steque is empty.");
+	}
+	else
+	{
+		//System.out.print("[");
+	while(temp!=null)
+	{
+		
+		System.out.print(temp.data);
+		temp=temp.next;
+		if(temp!=null)
+		System.out.print(", ");
+	}
+	//System.out.print("]");
+	}
+}
+public void pushfront(int a)
+{
+
+		Node n=new Node(a);
+	
+		if(head==null)
 		{
-			first=n;
+			head=n;
 			
 		}
 		else
-		{	
+		{	n.next=head;
+		    head=n;
+		    
+		}
 		
-		n.next=first;
-                 first=n;
-		}
-	}
-	public void enqueue(int item)
-	{
-		Node n=new Node(item);
-		
-		if(first==null)
-		{
-			first=n;
-			
-		}
-		else
-		{	
-               Node temp=first;
-               while(temp.next!=null)
-               {
-              temp=temp.next;
-		}
-              temp.next=n; 
-                } 
-	}
-	public int pop()
-	{
-		if(first==null)
+}
+public int popfront()
+{
+	
+		if(head==null)
 		{
 			return 0;
 		}
 		else
-		{
-		Node old=first;
-		first=first.next;
-		return old.item;
-	    }
-	}
-	public void display()
-	{	
-		Node temp=first;
-		if(first==null)
-		{
-			System.out.println("Steque is empty");
+		{	Node temp1=head;
+		head=head.next;
+		   return temp1.data;
 		}
-		else
-		{
-		while(temp!=null)
-		{
-			System.out.print(temp.item+"ppp");
-			temp=temp.next;
-			if(temp!=null)
-			System.out.print(", ");
-		}
-		System.out.println();
-		}
-	}
+		
 }
-public class Solution {
-public static void main(String args[])
+
+
+public int size()
+{Node temp1=head;
+int count=0;
+while(temp1!=null)
 {
-	Scanner s=new Scanner(System.in);
-	int n=s.nextInt();
-	String st1[]=new String[2];
-	String st=null;
-	int i;
-	for(i=0;i<=n;i++)
-	{
-		Link l=new Link();
-       do
-		{
-			st=s.nextLine();
-				st1=st.split(" ");
-				switch(st1[0])
-				{
-				case "push":
-			          l.push(Integer.parseInt(st1[1]));
-			          l.display();
-                
-				                 break;
-				case "enqueue":
-			          l.enqueue(Integer.parseInt(st1[1]));   
-			          l.display();
-			    //      System.out.println();
-			          break;
-				case "pop":
-					l.pop();
-					l.display();
-                  //  System.out.println();
-				    break;
-		        }
-		}
-       while(s.hasNext() && st.length()!=0);
-	   if(i!=0)
-		{
-	     System.out.println();
-		}
-		}
+	temp1=temp1.next;
+	count++;
+	
 }
+return count;
+	
 }
+public String toString() {
+	Node temp=head;
+	
+	return head+"";
+}
+
+}
+
+
+
+ class Node {
+int data;
+Node next;
+Node(int a)
+{
+	this.data=a;
+}
+@Override
+public String toString() {
+	return data +""+next;
+}
+
+}
+ 
