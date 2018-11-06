dbd5a5600a8b5ce4d9794b1aacd7df9a8a218fdf
saidhruthi <nsaidhruthi@gmail.com>
2018-11-06 16:17:26 +0530

    "check_style_score -> 3 of 3 passed. style score: 0/1 "

---
+++ m20/20.1 BST/Solution.java
@@ -0,0 +1,358 @@
+//package m19;
+import java.util.NoSuchElementException;
+import java.util.Stack;
+
+import java.util.Scanner;
+
+public class Solution {
+public static void main(String[] args)
+{
+	Scanner sc=new Scanner(System.in);
+	ordered o=new ordered();
+	while(sc.hasNext())
+	{
+		String s=sc.nextLine();
+		String a[]=s.split(",");
+		Book b =new Book();
+		
+		switch(a[0])
+		{
+		
+		case "put":
+			 b=new Book(a[1],a[2],Double.parseDouble(a[3]));
+			int x=Integer.parseInt(a[4]);
+			o.put(b, x);
+		break;
+		case "get":
+		  int h=o.get(a[1],o.root);
+		if(h==0)
+		  {
+			  System.out.println("null");
+	       }
+		  else
+			  System.out.println(h);
+		break;
+		case "max":
+			System.out.println(o.max().name+", "+o.max().author+", "+o.max().price);
+			break;
+		case "min":
+			System.out.println(o.min().name+", "+o.min().author+", "+o.min().price);
+			break;
+		case "select":
+			//System.out.println(a[1]);
+		//o.rank("IT")
+			System.out.println(o.select(Integer.parseInt(a[1])).name+", "+o.select(Integer.parseInt(a[1])).author+", "+o.select(Integer.parseInt(a[1])).price);
+		
+			break;
+		case "floor":
+			Book b1=o.floor(a[1]);
+			if(b1==null)
+			{
+				System.out.println("null");
+			}
+			else
+			{
+				System.out.println(b1.name+", "+b1.author+", "+b1.price);
+			}
+			//System.out.println(o.floor(a[1]).name+","+o.floor(a[1]).author+","+o.floor(a[1]).price);
+			break;
+		case "ceiling":
+			Book b11=o.ceiling(a[1]);
+			if(b11==null)
+			{
+				System.out.println("null");
+			}
+			else
+			{
+				System.out.println(b11.name+", "+b11.author+", "+b11.price);
+			}
+			//System.out.println(o.ceiling(a[1]).name+","+o.ceiling(a[1]).author+","+o.ceiling(a[1]).price);
+			break;
+		case "deleteMax":
+			Book b2=o.deleteMax(o.root).key;
+			//System.out.println(b2.name+","+b2.author+","+b2.price);
+			//System.out.println(.name+", "+o.deleteMax(o.root).key.author+", "+o.deleteMax(o.root).key.price);
+			//o.deleteMax(o.root);
+			//b.toString();
+			break;
+		case "deleteMin":
+	       // Book b3=o.deleteMin(o.root).key;
+	       o.deletemin();
+	       // System.out.println(b3.name+","+b3.author+","+b3.price);
+			break;
+		case "delete":
+			 b=new Book(a[1],a[2],Double.parseDouble(a[3]));
+							o.delete(b);
+			break;
+			
+			
+		}
+	}
+}
+}
+//package m19;
+
+class Book
+{
+	String name;
+	String author;
+	double price;
+	public Book(String name, String author, double price) 
+	{
+		this.name = name;
+		this.author = author;
+		this.price = price;
+	}
+	public Book() {
+		
+	}
+	
+	public int compareTo(Book key) {
+				if(this.name.equals(key.name))
+				{
+					return 0;
+				}
+				else if(this.name.compareTo(key.name)>0)
+				{
+					return 1;
+				}
+				else
+				return -1;
+	}
+	
+}
+ class ordered {
+Node root;
+	class Node
+{
+	int value;
+	Book key;
+	Node left,right;
+	int size;
+	public Node(Book key,int value,int size) {
+		
+		this.value = value;
+		this.key = key;
+		this.size=size;
+	}
+	
+}
+void put(Book k,int val)
+{
+	root=put(root,k,val);
+}
+Node put(Node root,Book k,int v)
+{
+	if(root==null)
+		return new Node(k,v,1);
+	int cmp=k.compareTo(root.key);
+	if(cmp<0)
+		root.left=put(root.left,k,v);
+	else if(cmp>0)
+		root.right=put(root.right,k,v);
+	else
+		root.value=v;
+	root.size=1+size(root.left)+size(root.right);
+	return root;
+}
+int get(String k,Node x)
+{
+	
+	while(x!=null)
+	{
+		//System.out.println("Name:"+x.key.name);
+		int cmp=k.compareTo(x.key.name);
+		if(cmp<0)
+			x=x.left;
+		else if(cmp>0)
+			x=x.right;
+		else
+			return x.value;
+	}
+	return 0;
+}
+Book floor(String k)
+{
+	Node x=floor(root,k);
+	if(x==null)
+		return null;
+	return x.key;
+}
+Node floor(Node x,String k)
+{
+	if(x==null)
+		return null;
+	int cmp=k.compareTo(x.key.name);
+	if(cmp==0)
+		return x;
+	if(cmp<0)
+		return floor(x.left,k);
+	Node t=floor(x.right,k);
+	if(t!=null)
+		return t;
+	else
+		return x;
+}
+public Book min()
+{
+	if(isEmpty())
+		throw new NoSuchElementException("calls min() with empty symbol table");
+	return min(root).key;
+}
+private Node min(Node x)
+{ 
+    if (x.left == null) 
+    	return x; 
+    else
+    	return min(x.left); 
+}
+public Book max()
+{
+	if(isEmpty())
+		throw new NoSuchElementException("calls max() with empty symbol table");
+	return max(root).key;
+}
+private Node max(Node x)
+{
+	if(x.right==null)
+		return x;
+	else
+		return max(x.right);
+}
+private boolean isEmpty() {
+	return false;
+}
+Book ceiling(String key)
+{
+	 Node x = ceiling(root, key);
+     if (x == null) return null;
+     else return x.key;
+}
+private Node ceiling(Node x,String k)
+{
+	if(x==null)
+		return null;
+	int cmp=k.compareTo(x.key.name);
+	if(cmp==0)
+		return x;
+	if(cmp<0)
+	{
+		Node t=ceiling(x.left,k);
+	   if(t!=null)
+		   return t;
+	   else return x;
+	}
+	return ceiling(x.right,k);
+}
+public Book select(int k) 
+{
+  // System.out.println("k"+k); 
+	Node x = select(root, k);
+    return x.key;
+} 
+ Node select(Node x, int k) {
+     
+    while(x!=null)
+    {
+    //System.out.println(x.left+"left");
+    int t = size(x.left); 
+    if      (t > k) return select(x.left,  k); 
+    else if (t < k) return select(x.right, k-t-1); 
+    else            return x; 
+	}
+    return null;
+ }
+public int rank(String key) {
+    if (key == null) throw new IllegalArgumentException("argument to rank() is null");
+    return rank(key, root);
+} 
+private int rank(String key, Node x) {
+    if (x == null) return 0; 
+    int cmp = key.compareTo(x.key.name); 
+    if      (cmp < 0) return rank(key, x.left); 
+    else if (cmp > 0) return 1 + size(x.left) + rank(key, x.right); 
+    else              return size(x.left); 
+}
+public int size() {
+    return size(root);
+}
+private int size(Node x) {
+    if (x == null)
+    	return 0;
+    else return x.size;
+}
+public Iterable<Book> keys() {
+    if (isEmpty()) return new Stack<Book>();
+    return keys(min(), max());
+}
+public Iterable<Book> keys(Book lo, Book hi) {
+    if (lo == null) throw new IllegalArgumentException("first argument to keys() is null");
+    if (hi == null) throw new IllegalArgumentException("second argument to keys() is null");
+
+    Stack<Book> q = new Stack<Book>();
+    keys(root, q, lo, hi);
+    return q;
+} 
+private void keys(Node x, Stack<Book> q, Book lo, Book hi) { 
+    if (x == null) return; 
+    int cmplo = lo.compareTo(x.key); 
+    int cmphi = hi.compareTo(x.key); 
+    if (cmplo < 0) keys(x.left, q, lo, hi); 
+    if (cmplo <= 0 && cmphi >= 0) q.push(x.key); 
+    if (cmphi > 0) keys(x.right, q, lo, hi); 
+}
+
+void deletemin()
+{
+	root=deleteMin(root);
+}
+
+
+Node deleteMin(Node x)
+{
+	if(x.left==null)
+		return x.right;
+	x.left=deleteMin(x.left);
+	root.size=1+size(x.left)+size(x.right);
+	return x;
+}
+
+Node  deleteMax(Node x)
+{
+	Node y=max(x);
+	
+	delete(y.key);
+	//System.out.println(y.key.name+",name,"+y.key.author+","+y.key.price);
+	return y;
+}
+
+public void delete(Book k)
+{
+	root=delete(root,k);
+	
+}
+Node delete(Node x,Book k)
+{
+	if(x==null)
+		return null;
+	int cmp=k.compareTo(x.key);
+	if(cmp<0) 
+		x.left=delete(x.left,k);
+	else if(cmp>0)
+		x.right=delete(x.right,k);
+	else
+	{
+		if(x.right==null)
+			return x.left;
+		Node t=x;
+		x=min(t.right);
+		x.right=deleteMin(t.right);
+		x.left=t.left;
+	}
+	x.size=size(x.left)+size(x.right)+1;
+	return x;
+}
+}
+
+
+
