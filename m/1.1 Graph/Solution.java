d99af68001e8712219d9415115ead7c5d19ca7c2
saidhruthi <nsaidhruthi@gmail.com>
2018-11-12 21:25:59 +0530

    "check_style_score -> 0 of 8 passed. style score: 0/1 "

---
+++ m/1.1 Graph/Solution.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Solution {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+     BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+	 String s=br.readLine();
+	 System.out.println("list "+s);
+		 switch(s)
+	 {
+	 case "List":
+		
+		 /*int ver=Integer.parseInt(br.readLine());
+		 System.out.println("v"+ver);
+		 int edg=Integer.parseInt(br.readLine());
+		 System.out.println("e"+edg);*/
+		 Graph g=new Graph(br);
+         System.out.println(g.toString());
+		
+		 break;
+	 case "Matrix":
+
+		 break; 
+	 }
+	}
+
+}
