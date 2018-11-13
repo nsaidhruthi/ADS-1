24b21ad5c84ee8b736c311f585faaf0d0dff4743
saidhruthi <nsaidhruthi@gmail.com>
2018-11-13 10:41:55 +0530

    "check_style_score -> 3 of 8 passed. style score: 0/1 "

--- m/1.1 Graph/Graph.java
+++ m/1.1 Graph/Graph.java
@@ -56,20 +56,12 @@ for(int i=0;i<E;i++)
 }
 void addEddge(int v,int w)
 {
-	if(!(v==w)||!duplicate(v,w))
+	if(!(v==w))
 	E++;
 	adj[v].add(w);
 	adj[w].add(v);
 }
-private boolean duplicate(int v2,int w) {
-	 for (int w1 : adj[v2]) {
-	        if(w1==w)
-	        {
-	        	return true;
-	        }
-        }
-	return false;
-}
+
 public String toString() {
     StringBuilder s = new StringBuilder();
     s.append(V + " vertices, " + E + " edges"+"\n");
